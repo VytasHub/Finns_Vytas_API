87f0304b08cf4a90a5bfcf470840384f6b5a7981
Brokenthought <2finnwilliams@gmail.com>
2015-11-19 18:56:02 +0000

    Changed over to new json files

--- index.js
+++ index.js
@@ -7,59 +7,55 @@ var fs = require('fs');
 
 
 
-
-
-
-
 var express = require('express')
    , app = module.exports = express()
    , nano = require('nano')('https://admin:a75695fd7905@couchdb-7c4dd5.smileupps.com')
    , request = require('request');
 
-//var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-
-var db_name = 'my_couch';
+var db_name= 'crimedb';
 
 var couchdb = nano.use(db_name);
 
 var local;
 
+//var data = fs.readFileSync('crime.json', 'utf8');
+//var json = JSON.parse(data);
 
-//console.log(data.dataset.dimension.Country);
-
-
-function getData(callback){
-    couchdb.get('this_is_the_document', { revs_info: true }, function (err, body) {
-        console.log("Error: " + err + "\nbody: " + body);
-        if (!err)
-
-            //console.log(body.dataset.dimension.Country);
-            data = body;
-            
-            local = true;
-            callback();
-       
-    });
-
-}
-
-//console.log(get_type(data));
-//nano.db.destroy(db_name, function () {
+//nano.db.destroy('crimedb', function () {
 //    // create a new database
-//    nano.db.create(db_name, function () {
+//    nano.db.create('crimedb', function () {
 //        // specify the database we are going to use
-//        var couchdb = nano.use(db_name);
+//        var alice = nano.use('crimedb');
 //        // and insert a document in it
-//        couchdb.insert(data, 'this_is_the_document', function (err, body, header) {
+//        alice.insert(json, 'crime', function (err, body, header) {
 //            if (err) {
-//                console.log('what?', err.message);
+//                console.log('[alice.insert] ', err.message);
 //                return;
 //            }
 //            console.log('you have inserted the rabbit.')
 //            console.log(body);
 //        });
+
+
 //    });
 //});
+//console.log(data.dataset.dimension.Country);
+
+var data;
+
+
+function getData(callback) {
+
+    couchdb.get('income', { revs_info: true }, function (err, body) {
+        console.log(err + "\nbody: " + body);
+        if (!err)
+            data = body;     
+            local = true;
+            callback();     
+    });
+
+}
+
 
 app.set('view engine', 'jade');
 
@@ -82,16 +78,31 @@ app.get('/crimeco/counties/:id', function (request, response) {
     {
         getData(function () {
 
-                console.log('callback');
+            response.send(data.dataset.dimension["County and Region"].category.label[request.params.id]);
+                
             });        
     }
     else {
-        result = "local";//data.dataset.dimension.Country[request.params.id];
+        response.send(data.dataset.dimension["County and Region"].category.label[request.params.id]);
     }
 
     console.log(result);
 
     response.send(result);
+    //response.status(200).json(request.params.id);
+});
+
+app.get('/crimeco/counties/', function (request, response) {
+
+    if (!local) {
+        getData(function () {
+            response.send(data.dataset.dimension["County and Region"].category.label);
+        });
+    }
+    else {
+        response.send(data.dataset.dimension["County and Region"].category.label);
+    }
+
     //response.status(200).json(request.params.id);
 });
 
