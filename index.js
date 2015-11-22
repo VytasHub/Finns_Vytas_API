509a0327425fdee3c88a7c561d2b5ee1525d12bc
VytasHub <vytasvaiciulisgti@gmail.com>
2015-11-22 00:09:06 +0000

    Angular View
    Angular view

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ var express = require('express')
    , nano = require('nano')('https://admin:a75695fd7905@couchdb-7c4dd5.smileupps.com')
    , request = require('request');
 
-var cors = require('cors');
-var qs = require('querystring');
 var db_name= 'crimedb';
 
 var couchdb = nano.use(db_name);
@@ -41,8 +39,6 @@ var local;
 
 var data;
 
-app.use(cors());
-
 
 function getData(callback) {
 
@@ -69,7 +65,7 @@ app.get("/crimeco", function (request, response) {
 });
  
 
-app.get("/crimeco/countiespost", function (request, response) {
+app.get("/countiespost", function (request, response) {
     response.contentType('text/html');
     response.status(200).sendFile(path.join(__dirname + '/views/counties.html'));
 });
@@ -111,56 +107,32 @@ app.get('/crimeco/counties/:id', function (request, response) {
 
 
 app.get('/crimeco/counties/', function (request, response) {
- 
+    
+    //countys = [];
+    //parsedCountys = [];
+
     if(data != undefined)
+    {
         response.send(data.dataset.dimension["County and Region"].category.label);
+    }
+
+
+
+        
     else
         response.send("No Data :/");
 });
 
 
 
-app.post('/crimeco/countiespost/', function (request, response) {
+app.post('/countiespost/', function (request, response) {
+    console.log("you posted" + request.param.variable_name);
 
-    var body = '';
-
-    request.on('data', function (data) {
-        body += data;
-    });
-
-
-
-    request.on('end', function () {
-        var post = JSON.parse(body);
-
-        console.log("Crime: " + post.crime + "   Location: " + post.location);
-    });
-    
+    console.log(request.body.crime);     // your JSON
+    response.send(request.body);    // echo the result back
 });
 
-app.post('/login', function (request, res) {
 
-
-    var body = '';
-
-
-
-
-    //var user_name = JSON.parse(req.body);
-    ////var user_name2 = JSON.stringify(req);
-    ////var test = req.body.name;
-    ////console.log(user_name);
-    //if (req == undefined) {
-    //    console.log('Fucked');
-    //}
-
-    //console.log(user_name);
-    //console.log(user_name2);
-    //console.log(JSON.stringify(test));
-    //var password = JSON.stringify(req.body.password);
-    //console.log("User name = " + user_name + ", password is " + password);
-
-});
 
 app.listen(3333);
 console.log("server is on port 3333");
